2247467f5a00160c5fdc312aed6ad2519c48707d
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2025-04-09 00:00:31 +0200

    implement basic Trusted App before implementing full test
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

---
+++ security_test/host/main.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2016, Linaro Limited
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <err.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* OP-TEE TEE client API (built by optee_client) */
+#include <tee_client_api.h>
+
+/* For the UUID (found in the TA's h-file(s)) */
+#include <security_test_ta.h>
+
+#define LLC_SIZE 1024*1024
+#define SEC_TO_NS(sec) ((sec)*1000000000)
+
+void flush_cache() {
+	static const int allocate_amount = LLC_SIZE * 10;
+	int* data = malloc(allocate_amount * sizeof(int));
+	for (int i = 0; i < allocate_amount; ++i) {
+		data[i] = i;
+	}
+}
+
+uint64_t nanosec() {
+	struct timespec ts;
+	if (clock_gettime(CLOCK_MONOTONIC_RAW, &ts) == -1) {
+		return -1;
+	}
+	return SEC_TO_NS((uint64_t)ts.tv_sec) + (uint64_t)ts.tv_nsec;
+}
+
+int main(void)
+{
+	TEEC_Result res;
+	TEEC_Context ctx;
+	TEEC_Session sess;
+	TEEC_Operation op;
+	TEEC_UUID uuid = TA_SECURITY_TEST_UUID;
+	uint32_t err_origin;
+	uint64_t t1, t2;
+
+	/* Initialize a context connecting us to the TEE */
+	res = TEEC_InitializeContext(NULL, &ctx);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InitializeContext failed with code 0x%x", res);
+
+	/* Allocate 10 MB of shared memory */
+	TEEC_SharedMemory shm = { };
+	shm.size = 1024*1024*10;
+	shm.flags = TEEC_MEM_INPUT | TEEC_MEM_OUTPUT;
+	printf("Allocating %d bytes of shared memory\n", shm.size);
+	printf("TEEC_CONFIG_SHAREDMEM_MAX_SIZE=%d\n", TEEC_CONFIG_SHAREDMEM_MAX_SIZE);
+	TEEC_AllocateSharedMemory(&ctx, &shm);
+
+	/*
+	 * Open a session to the "security test" TA
+	 */
+	res = TEEC_OpenSession(&ctx, &sess, &uuid,
+			       TEEC_LOGIN_PUBLIC, NULL, NULL, &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_Opensession failed with code 0x%x origin 0x%x",
+			res, err_origin);
+
+	/*
+	 * Execute a function in the TA by invoking it, in this case
+	 * we're incrementing a number.
+	 *
+	 * The value of command ID part and how the parameters are
+	 * interpreted is part of the interface provided by the TA.
+	 */
+
+	/* Clear the TEEC_Operation struct */
+	memset(&op, 0, sizeof(op));
+
+	/*
+	 * Prepare the argument. Pass a value in the first parameter,
+	 * the remaining three parameters are unused.
+	 */
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT, TEEC_NONE,
+					 TEEC_NONE, TEEC_NONE);
+	op.params[0].memref.parent = shm.buffer;
+	op.params[0].memref.size = shm.size;
+
+	printf("Flush cache & invoke TA command\n");
+	flush_cache();
+
+	/*
+	 * TA_SECURITY_TEST_CMD_READ_MEM is the actual function in the TA to be
+	 * called.
+	 */
+	res = TEEC_InvokeCommand(&sess, TA_SECURITY_TEST_CMD_READ_MEM, &op,
+				 &err_origin);
+	if (res != TEEC_SUCCESS)
+		errx(1, "TEEC_InvokeCommand failed with code 0x%x origin 0x%x",
+			res, err_origin);
+
+
+	t1 = nanosec();
+	((int*)shm.buffer)[0] = 1;
+	t2 = nanosec();
+	printf("Shared mem read time = %d\n", t2 - t1);
+
+	/*
+	 * We're done with the TA, close the session and
+	 * destroy the context.
+	 */
+
+	TEEC_CloseSession(&sess);
+
+	TEEC_FinalizeContext(&ctx);
+
+	return 0;
+}
